57f5e31f9d5bad992d2b958694a39f201a4728f0
babuece97 <babuece97@yahoo.com.com>
2021-04-07 01:04:12 +0200

    routeV2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,50 @@
 import './App.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Order from './components/Order/Order';
 import Admin from './components/Admin/Admin';
 import Login from './components/Login/Login';
 import Review from './components/Review/Review';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-        <Container>
-       <h1> Welcome to Subhah's world</h1>
-       </Container>
-       
-       <div>
-       <Header></Header>
-         
-      <Router>
-        
-         <Switch>
-         <Route path="/home">
-             <Home></Home>
-           </Route>
+      <Container>
+        <h1> Welcome to Subhah's world</h1>
+      </Container>
 
-           <Route path="/review">
-             <Review></Review>
-           </Route>
-           <Route path="/order">
-              <Order></Order>
-            </Route>
-            <Route path="/admin">
-              <Admin></Admin>
-            </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/">
+      <div>
+        <Header></Header>
+
+        <Router>
+          <Switch>
+            <Route path='/home'>
               <Home></Home>
             </Route>
-         </Switch>
-      </Router>
+
+            <Route path='/review'>
+              <Review></Review>
+            </Route>
+            <Route path='/order'>
+              <Order></Order>
+            </Route>
+            <Route path='/admin'>
+              <Admin></Admin>
+            </Route>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='*'>
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </>
   );
